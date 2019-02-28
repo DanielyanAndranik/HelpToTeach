9bbdce20d93e74dff2cf90fc829b3f028a4aace7
Andranik <danielyandranik@gmail.com>
2019-02-28 15:52:26 +0400

    Add models

---
+++ WebApplication/Migrations/20190228110346_Third.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WebApplication.Migrations
+{
+    public partial class Third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Role",
+                table: "Users",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "GroupCourses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    CouchbaseId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupCourses_CourseId",
+                table: "GroupCourses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupCourses_GroupId",
+                table: "GroupCourses",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupCourses_UserId",
+                table: "GroupCourses",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GroupId",
+                table: "Students",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupCourses_Courses_CourseId",
+                table: "GroupCourses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupCourses_Groups_GroupId",
+                table: "GroupCourses",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupCourses_Users_UserId",
+                table: "GroupCourses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupCourses_Courses_CourseId",
+                table: "GroupCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupCourses_Groups_GroupId",
+                table: "GroupCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupCourses_Users_UserId",
+                table: "GroupCourses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GroupCourses_CourseId",
+                table: "GroupCourses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GroupCourses_GroupId",
+                table: "GroupCourses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GroupCourses_UserId",
+                table: "GroupCourses");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "GroupCourses");
+        }
+    }
+}
--- WebApplication/Models/HelpToTeachContext.cs
+++ WebApplication/Models/HelpToTeachContext.cs
@@ -8,40 +8,61 @@ namespace WebApplication.Models
 {
     public class HelpToTeachContext : DbContext
     {
-        public HelpToTeachContext(DbContextOptions<HelpToTeachContext> options) : base(options)
-        {
-        }
+        public HelpToTeachContext(DbContextOptions<HelpToTeachContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
         public DbSet<Group> Groups { get; set; }
         public DbSet<GroupCourse> GroupCourses { get; set; }
     }
 
     public class User
     {
-        
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public byte Role { get; set; }
+        public List<GroupCourse> GroupCourses { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+        public int CouchbaseId { get; set; }
     }
 
     public class Course
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<GroupCourse> GroupCourses { get; set; }
     }
 
     public class Group
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public List<GroupCourse> GroupCourses { get; set; }
     }
 
     public class GroupCourse
     {
         public int Id { get; set; }
         public int GroupId { get; set; }
+        public Group Group { get; set; }
         public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
