7c5e3453ff50f32718865e04e72f8393d0c5a93f
Ghevond <ghevond.98@gmail.com>
2019-03-03 19:55:18 +0400

    the web api has been added

---
+++ WebApi/Configuration/CouchbaseConfig.cs
@@ -0,0 +1,45 @@
+﻿using Couchbase;
+using Couchbase.Configuration.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Configuration
+{
+    public class CouchbaseConfig
+    {
+
+        private const string ServerUri = "http://127.0.0.1:8091";
+        private const string BucketName = "HelpToTeachMainBucket";
+        private const string BucketPass = "password";
+
+        public static void Setup()
+        {
+            var config = new ClientConfiguration
+            {
+
+                Servers = new List<Uri> {
+                    new Uri(ServerUri)
+                },
+                UseSsl = false,
+                BucketConfigs = new Dictionary<string, BucketConfiguration>
+                {
+                    {
+                        "HelpToTeachMainBucket",new BucketConfiguration
+                        {
+                            BucketName = BucketName,
+                            Password = BucketPass,
+                            UseSsl = false
+                        }
+                    }
+                }
+            };
+            ClusterHelper.Initialize(config);
+        }
+        public static void Cleanup()
+        {
+            ClusterHelper.Close();
+        }
+    }
+}
---
+++ WebApi/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly IRepository<Student> _studentsRepository = new CouchbaseRepository<Student>();
+
+
+        #region GET
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            try
+            {
+                var students = await _studentsRepository.GetAll(typeof(Student));
+                return Ok(students);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500,e);
+            }
+        }
+
+        #endregion
+
+    }
+}
