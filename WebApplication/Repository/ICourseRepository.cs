47087270556e65d51fd13f7b0ced1d404fc466fa
Andranik <danielyandranik@gmail.com>
2019-03-12 18:58:32 +0400

    Added dashboard

---
+++ WebApplication/Repository/ICourseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Repository
+{
+    public interface ICourseRepository
+    {
+        Task<IEnumerable<Group>> GetAll();
+        Task<Group> Get(string id);
+        Task<IEnumerable<Group>> GetByLecturer(string id);
+        Task<Group> Create(Group course);
+        Task<Group> Update(Group course);
+        Task<Group> Upsert(Group course);
+        Task Delete(string id);
+    }
+}
