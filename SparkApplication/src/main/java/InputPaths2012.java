55bcdf303fdf4af2bb819e74e9d657758063a1a6
Ghevond <ghevond.98@gmail.com>
2019-04-30 22:13:46 +0400

    txt to csv

--- SparkApplication/src/main/java/InputPaths2012.java
+++ SparkApplication/src/main/java/InputPaths2012.java
@@ -1,8 +1,8 @@
 public class InputPaths2012 {
-    public final static String DotNet = "input/2012/Students-.Net.txt";
-    public final static String Calculus = "input/2012/Students-Calculus.txt";
-    public final static String ImageProcessing = "input/2012/Students-ImageProcessing.txt";
-    public final static String MachineLearning = "input//2012/Students-MachineLearning.txt";
-    public final static String Java = "input/2012/Students-Java.txt";
-    public final static String SQL = "input/2012/Students-SQL.txt";
+    public final static String DotNet = "input/2012/Students-.Net.csv";
+    public final static String Calculus = "input/2012/Students-Calculus.csv";
+    public final static String ImageProcessing = "input/2012/Students-ImageProcessing.csv";
+    public final static String MachineLearning = "input//2012/Students-MachineLearning.csv";
+    public final static String Java = "input/2012/Students-Java.csv";
+    public final static String SQL = "input/2012/Students-SQL.csv";
 }
